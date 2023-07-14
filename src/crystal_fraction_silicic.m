5cdd5bdb7b1a80046c8d14ea7627cf5e4a100efe
kjscholz <scholz.kathryn@gmail.com>
2023-07-13 18:20:00 -0700

    more cleaning up commenting in src

--- src/crystal_fraction_silicic.m
+++ src/crystal_fraction_silicic.m
@@ -1,8 +1,9 @@
 function [eps_x, deps_x_dP, deps_x_dT,deps_x_deps_g, deps_x_dmco2_t, deps_x_dmh2o_t]= crystal_fraction_silicic(T,P, eps_g,X_co2,mH2O,mCO2,rho_m,rho_x,rho_g)
 
-%% NEW VERSION WITH SAGE's PARAMETERIZATION
+% convert T to Celcius from Kelvin
 T=T-273;
 
+%compute coefficients
 [a dadx  dady dadz b dbdx dbdy dbdz c dcdx dcdy dcdz]=parameters_melting_curve_silicic(100*mH2O,100*mCO2,P);
 
 if T < 400
@@ -12,9 +13,10 @@ end
     
     
 eps_x=a*erfc(b*(T-c));
+
 % derivatives
 deps_x_deps_g=-1;
-deps_x_dT=-2*a*b*exp(-b^2*(T-c)^2)/sqrt(pi); % 
+deps_x_dT=-2*a*b*exp(-b^2*(T-c)^2)/sqrt(pi); 
 deps_x_dP=dadz*erfc(b*(T-c))-2*a*(T-c)/sqrt(pi)*exp(-b^2*(T-c)^2)*dbdz+2*a*b/sqrt(pi)*exp(-b^2*(T-c)^2)*dcdz;
 deps_x_dmco2_t=dady*erfc(b*(T-c))-2*a*(T-c)/sqrt(pi)*exp(-b^2*(T-c)^2)*dbdy+2*a*b/sqrt(pi)*exp(-b^2*(T-c)^2)*dcdy;
 deps_x_dmh2o_t=dadx*erfc(b*(T-c))-2*a*(T-c)/sqrt(pi)*exp(-b^2*(T-c)^2)*dbdx+2*a*b/sqrt(pi)*exp(-b^2*(T-c)^2)*dcdx;
--- src/eos_g.m
+++ src/eos_g.m
@@ -1,12 +1,14 @@
 function [rho_g,drho_g_dP,drho_g_dT] = eos_g(P,T)
 
 
-
 % parametrization of redlich kwong taken from Huber et al. 2010
 rho_g        = -112.528*(T-273.15)^-0.381 + 127.811*(P*1e-5)^-1.135 + 112.04*(T-273.15)^-0.411*(P*1e-5)^0.033; 
 drho_g_dP    = (-1.135)*127.811*(P*1e-5)^-2.135 + 0.033*112.04*(T-273.15)^-0.411*(P*1e-5)^-0.967;
 drho_g_dT    = (-0.381)*(-112.528)*(T-273.15)^-1.381 + (-0.411)*112.04*(T-273.15)^-1.411*(P*1e-5)^0.033;
 
-rho_g        = rho_g*1e3; %display(rho_g)
-drho_g_dP    = drho_g_dP*1e-2;%display(drho_gdP)
-drho_g_dT    = drho_g_dT*1e3;%display(drho_gdT)
+% convert to kg/m3
+rho_g        = rho_g*1e3; 
+% convert to kg/m3/Pa
+drho_g_dP    = drho_g_dP*1e-2;
+% convert to kg/m3/K
+drho_g_dT    = drho_g_dT*1e3;
